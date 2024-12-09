d6a8225feb3e14ff2a935e2e7058397e6bcc1b0a
Expedit206 <mciagnessi@gmail.com>
2024-12-09 06:18:37 +0100

    vid7 fini with get tabs order

--- app/Filament/Admin/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Admin/Resources/OrderResource/Pages/ListOrders.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Admin\Resources\OrderResource\Pages;
 
 use App\Filament\Admin\Resources\OrderResource;
+use App\Filament\Admin\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Actions\CreateAction;
 
 class ListOrders extends ListRecords
 {
@@ -14,6 +17,32 @@ class ListOrders extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
-        ];
+        ];  
     }
+
+    public function getHeaderWidgets():array
+    {
+        return[
+            OrderStats::class
+        ];
+     }
+
+    // public function getFooterWidgets():array
+    // {
+    //     return[
+    //         OrderStats::class
+    //     ];
+    //  }
+
+    public function getTabs():array
+    {
+        return[
+            null=>Tab::make('All'),
+            'new '=>Tab::make()->query(fn ($query)=> $query->where('status', 'new')),
+            'processing '=>Tab::make()->query(fn ($query)=> $query->where('status', 'processing')),
+            'shipped '=>Tab::make()->query(fn ($query)=> $query->where('status', 'shipped')),
+            'delivered '=>Tab::make()->query(fn ($query)=> $query->where('status', 'delivered')),
+            'cancelled '=>Tab::make()->query(fn ($query)=> $query->where('status', 'cancelled')),
+        ];
+     }
 }
